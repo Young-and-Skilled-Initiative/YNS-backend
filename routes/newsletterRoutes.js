50477ab85603651f8e00d41e6d0034ac36bcb58b
davedumto <ejeredavid2001@gmail.com>
2025-05-26 16:43:46 +0100

    feat: adding features for the Cms

--- routes/newsletterRoutes.js
+++ routes/newsletterRoutes.js
@@ -1,11 +1,14 @@
 const express = require("express");
-const { subscribeToNewsletter, sendBulkNewsletter } = require("../controller/newsletterController");
+const { 
+  subscribeToNewsletter, 
+  sendBulkNewsletter, 
+  getNewsletterStats 
+} = require("../controller/newsletterController");
 
 const router = express.Router();
 
-
 router.post("/subscribe", subscribeToNewsletter);
-
 router.post("/send-newsletter", sendBulkNewsletter);
+router.get("/stats", getNewsletterStats);
 
 module.exports = router;
